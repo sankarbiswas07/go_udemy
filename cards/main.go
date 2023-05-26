eef8526a95f206fde50e2b0e3406fb71b17db131
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-27 00:23:36 +0530

    19. custom type declaration

--- cards/main.go
+++ cards/main.go
@@ -10,10 +10,14 @@ func main() {
 	// In Go: there are two types of array - 1. fixed size & 2. slice(fancy name of a array which can grow or shrink)
 	// here we are writing slice and iterate over it
 
-	cards := [] string { "Ace of Heart", newCard()}
+	cards := deck { "Ace of Heart", newCard()}
 	cards = append(cards, "Four of Heart")
 
 	for i, card := range cards {
 	 fmt.Println(i, card)
 	}
 }
+
+
+// go run main.go deck.go
+// Here we have made a custom dec type which is actually a slice of type string
