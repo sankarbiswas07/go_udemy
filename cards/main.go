417a67c5ed6dbe425d8b417278e57922a9f4e6fc
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-26 18:05:09 +0530

    16. Functions and Return Types

--- cards/main.go
+++ cards/main.go
@@ -2,18 +2,12 @@ package main
 
 import "fmt"
 
+func newCard() string {
+	return "Five of Diamonds"
+}
+
 func main() {
-	// var card string = "Ace of Spread"
-	// the above line is equivalent to the following line
-
-	// := telling go , to figure ut what type of data we are assigning to it
-	// in case of the following. it will set a string type
-	// := only valid while the initialization, no re-init state.
-
-	card := "Ace of Spread"
-
-	// Re-assign with equal to operator
-	card = "Fives diamonds"
+	card := newCard()
 
 	fmt.Println(card)
 }
