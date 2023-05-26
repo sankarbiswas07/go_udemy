8a5d147ad062706c46407aa7b67254c771313c8d
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-26 17:52:55 +0530

    Variable decalaration

--- helloworld/main.go
+++ helloworld/main.go
@@ -2,6 +2,17 @@
 
 package main
 
+/**
+* QUESTIONS? => Answer
+*
+*	1. How do we run the code in our project? => go command line tool. either we go to project directory (helloworld) > go run main.go or we can build (go build main.go) it and ./ main
+* 2. What does "package main" mean? => if we have multiple file and relates each other. wrap it within main. package main
+* 3. What does "import 'fmt'" mean? => default the package main has no connection with any of the packages. "fmt" is the standard lib of go
+* 4. What is "func" thing? => func is keyword for function
+* 5. How is the main.go file organized?
+*
+ */
+
 import "fmt"
 
 func main() {
