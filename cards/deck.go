67873bcfa89348529d85b20f9427f9821e33ed9e
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-27 01:05:28 +0530

    21. Creating a New Deck

--- cards/deck.go
+++ cards/deck.go
@@ -4,12 +4,27 @@ import "fmt"
 
 type deck []string
 
+// Making a function which will make the cards list
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuites:=[] string {"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues:=[] string {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Joker", "Queen", "King"}
+
+	for _,suit := range cardSuites{
+		for _,value := range cardValues{
+			cards = append(cards, value+" of "+suit)		
+		}
+	}
+
+	return cards
+}
+
 // here "d" is the receiver
 // Actual copy of the deck we'er working with is d
 // (d deck) deck means, every deck type variable will have print() method
 // d can be cards also. but by convention it is the first letter or first a few latter as variable name
 // in go, it good practice ot have first letter d from deck name
-
 
 // like OO approach
 // deck class will have a deck instance with will have print() method
--- cards/main.go
+++ cards/main.go
@@ -1,18 +1,9 @@
 package main
 
-// import "fmt"
-
-func newCard() string {
-	return "Five of Diamonds"
-}
 
 func main() {
-	// In Go: there are two types of array - 1. fixed size & 2. slice(fancy name of a array which can grow or shrink)
-	// here we are writing slice and iterate over it
 
-	cards := deck { "Ace of Heart", newCard()}
-	cards = append(cards, "Four of Heart")
-
+	cards:= newDeck()
 	cards.print()
 
 }
