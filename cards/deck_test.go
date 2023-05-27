7056755572a233edbfa680761eb5ebbaf203ff84
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-28 01:17:08 +0530

    34. Asserting Elements in a Slice | Some more very basic test

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -8,4 +8,13 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 20, but got %v", len(d))
 	}
+	
+	if d[1] != "Two of Spades" {
+		t.Errorf("Expected second card as Two of Spades, but got %v", d[1])
+	}
+
+	if d[len(d) - 1] != "King of Clubs" {
+		t.Errorf("Expected last card as King of Clubs, but got %v", d[1])
+	}
+
 }
