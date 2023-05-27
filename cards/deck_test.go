b256fae2248961a97e6df02806380190ee67953d
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-28 01:34:47 +0530

    35. Testing File I/O

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
+
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -17,4 +21,22 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected last card as King of Clubs, but got %v", d[1])
 	}
 
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T){
+	// remove if exists any previous test file
+	os.Remove("__test_my_deck")
+
+	d := newDeck()
+	d.saveToFile("__test_my_deck")
+
+	deck := newDeckFromFile("__test_my_deck")
+
+	if(len(deck) != 52) {
+		t.Errorf("Expected saved deck has 52 cards, but got %v", len(deck))
+	}
+
+
+		// remove newly created test file
+		os.Remove("__test_my_deck")
 }
