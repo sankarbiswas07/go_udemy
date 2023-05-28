9baf77fe34f9ce0274ab0beef7bfadd780890819
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-28 15:23:46 +0530

    42. refinement and code moved to readme for reference. | we can only whrite type name

--- sec4_structs/main.go
+++ sec4_structs/main.go
@@ -6,7 +6,7 @@ import (
 // previous similar syntax
 // type deck []string
 
-type contactInfo struct {
+type contact struct {
 	email string
 	phone string
 }
@@ -15,41 +15,16 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName string
-	contact contactInfo
+	contact
 }
 
 
 func main(){
 
-	// // WAY:1 - to create struct, arguments will create struct in a sequential order.
-	// // firstName "Prasad Biswas" && lastName "Sankar" IF fullName_1 := person{"Prasad Biswas", "Sankar"}
-	// fullName_1 := person{"Sankar", "Prasad Biswas"}
-	// fmt.Println(fullName_1)
-
-	// // WAY:2 - to create struct, arguments will create struct by mentioning key.
-	// fullName_2 := person{firstName:"Sankar", lastName:"Prasad Biswas"}
-	// fmt.Println(fullName_2)
-
-	// // WAY:3 - to create struct, create a variable of type person
-	// // this actually means there is all the keys with "" empty string
-	// var fullName_3 person
-	// fmt.Println(fullName_3)
-	// fmt.Printf("%+v \n", fullName_3)
-	// // UPDATE
-	// fullName_3.firstName = "Sankar"
-	// fullName_3.lastName = "Prasad Biswas"
-	// fmt.Println(fullName_3)
-	// fmt.Printf("%+v \n", fullName_3)
-
-	// 41. Embedding Structs
-	// This is nothing but nested objects
-	// step 1: like person, you have to create one struct "type"
-	// step 2: make a key in the parent object of that particular struct "type"(step 1)
-
 	fullName_4 := person{
 		firstName:"Sankar",
 		lastName:"Prasad Biswas",
-		contact: contactInfo{
+		contact: contact{
 			email: "sankarbiswas07@gmail.com",
 			phone: "+91 8961766682",
 		},
--- sec4_structs/readme.md
+++ sec4_structs/readme.md
@@ -1,3 +1,37 @@
 Structs:
   - similar to Simple Object in JavaScript.
   - Organize data with Struct
+
+when
+
+// type person which is type struct, a simple object
+type person struct {
+	firstName string
+	lastName string
+}
+
+
+	// // WAY:1 - to create struct, arguments will create struct in a sequential order.
+	// // firstName "Prasad Biswas" && lastName "Sankar" IF fullName_1 := person{"Prasad Biswas", "Sankar"}
+	// fullName_1 := person{"Sankar", "Prasad Biswas"}
+	// fmt.Println(fullName_1)
+
+	// // WAY:2 - to create struct, arguments will create struct by mentioning key.
+	// fullName_2 := person{firstName:"Sankar", lastName:"Prasad Biswas"}
+	// fmt.Println(fullName_2)
+
+	// // WAY:3 - to create struct, create a variable of type person
+	// // this actually means there is all the keys with "" empty string
+	// var fullName_3 person
+	// fmt.Println(fullName_3)
+	// fmt.Printf("%+v \n", fullName_3)
+	// // UPDATE
+	// fullName_3.firstName = "Sankar"
+	// fullName_3.lastName = "Prasad Biswas"
+	// fmt.Println(fullName_3)
+	// fmt.Printf("%+v \n", fullName_3)
+
+	// 41. Embedding Structs
+	// This is nothing but nested objects
+	// step 1: like person, you have to create one struct "type"
+	// step 2: make a key in the parent object of that particular struct "type"(step 1)
