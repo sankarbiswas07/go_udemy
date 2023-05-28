6f6ded2502b97815ad2eaaff2d61066aba73ad28
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-28 13:32:21 +0530

    complete

--- Assigment2_even_odd/main.go
+++ Assigment2_even_odd/main.go
@@ -1,10 +1,21 @@
-
 package main
 
 import (
 	"fmt"
 )
 
+
 func main()  {
-	fmt.Println("hello")
+
+	slice := []int{0,1,2,3,4,5,6,7,8,9,10}
+
+	for _, n := range slice {
+		whatType := "even"
+		if n % 2 == 0 {
+			whatType = "even"
+		}else{
+			whatType = "odd"
+		}
+		fmt.Printf("%v is %v \n", n, whatType)
+	}
 }
