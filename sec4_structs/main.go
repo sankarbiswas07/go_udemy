e29301c9699f33f623d2a7129e29376a817e46ce
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-28 14:46:35 +0530

    41. what is embedding structs?

--- sec4_structs/main.go
+++ sec4_structs/main.go
@@ -16,7 +16,6 @@ func main(){
 
 	// WAY:1 - to create struct, arguments will create struct in a sequential order.
 	// firstName "Prasad Biswas" && lastName "Sankar" IF fullName_1 := person{"Prasad Biswas", "Sankar"}
-	
 	fullName_1 := person{"Sankar", "Prasad Biswas"}
 	fmt.Println(fullName_1)
 
@@ -26,16 +25,17 @@ func main(){
 
 	// WAY:3 - to create struct, create a variable of type person
 	// this actually means there is all the keys with "" empty string
-
 	var fullName_3 person
 	fmt.Println(fullName_3)
 	fmt.Printf("%+v \n", fullName_3)
-	
 	// UPDATE
 	fullName_3.firstName = "Sankar"
 	fullName_3.lastName = "Prasad Biswas"
-
 	fmt.Println(fullName_3)
 	fmt.Printf("%+v \n", fullName_3)
 
+	// 41. Embedding Structs
+	// This is nothing but nested objects
+	// step 1: like person, you have to create one struct "type"
+	// step 2: make a key in the parent object of that particular struct "type"(step 1)
 }
