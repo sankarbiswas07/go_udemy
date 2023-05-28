d218585cea018f5ef4702b690b73b944f35ec1f3
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-28 15:02:37 +0530

    41. Embedding Structs : all fields are required in this way

--- sec4_structs/main.go
+++ sec4_structs/main.go
@@ -6,36 +6,55 @@ import (
 // previous similar syntax
 // type deck []string
 
+type contactInfo struct {
+	email string
+	phone string
+}
+
 // type person which is type struct, a simple object
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
+
 
 func main(){
 
-	// WAY:1 - to create struct, arguments will create struct in a sequential order.
-	// firstName "Prasad Biswas" && lastName "Sankar" IF fullName_1 := person{"Prasad Biswas", "Sankar"}
-	fullName_1 := person{"Sankar", "Prasad Biswas"}
-	fmt.Println(fullName_1)
+	// // WAY:1 - to create struct, arguments will create struct in a sequential order.
+	// // firstName "Prasad Biswas" && lastName "Sankar" IF fullName_1 := person{"Prasad Biswas", "Sankar"}
+	// fullName_1 := person{"Sankar", "Prasad Biswas"}
+	// fmt.Println(fullName_1)
 
-	// WAY:2 - to create struct, arguments will create struct by mentioning key.
-	fullName_2 := person{firstName:"Sankar", lastName:"Prasad Biswas"}
-	fmt.Println(fullName_2)
+	// // WAY:2 - to create struct, arguments will create struct by mentioning key.
+	// fullName_2 := person{firstName:"Sankar", lastName:"Prasad Biswas"}
+	// fmt.Println(fullName_2)
 
-	// WAY:3 - to create struct, create a variable of type person
-	// this actually means there is all the keys with "" empty string
-	var fullName_3 person
-	fmt.Println(fullName_3)
-	fmt.Printf("%+v \n", fullName_3)
-	// UPDATE
-	fullName_3.firstName = "Sankar"
-	fullName_3.lastName = "Prasad Biswas"
-	fmt.Println(fullName_3)
-	fmt.Printf("%+v \n", fullName_3)
+	// // WAY:3 - to create struct, create a variable of type person
+	// // this actually means there is all the keys with "" empty string
+	// var fullName_3 person
+	// fmt.Println(fullName_3)
+	// fmt.Printf("%+v \n", fullName_3)
+	// // UPDATE
+	// fullName_3.firstName = "Sankar"
+	// fullName_3.lastName = "Prasad Biswas"
+	// fmt.Println(fullName_3)
+	// fmt.Printf("%+v \n", fullName_3)
 
 	// 41. Embedding Structs
 	// This is nothing but nested objects
 	// step 1: like person, you have to create one struct "type"
 	// step 2: make a key in the parent object of that particular struct "type"(step 1)
+
+	fullName_4 := person{
+		firstName:"Sankar",
+		lastName:"Prasad Biswas",
+		contact: contactInfo{
+			email: "sankarbiswas07@gmail.com",
+			phone: "+91 8961766682",
+		},
+	}
+	fmt.Println(fullName_4)
+	fmt.Printf("%+v \n", fullName_4)
+
 }
