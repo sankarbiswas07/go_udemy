0ec5d546a3ae02a86de2b5bedf4f59b31609e947
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-05-31 17:01:30 +0530

    56. Interface in practice : hands on

--- sec6_interfaces/main.go
+++ sec6_interfaces/main.go
@@ -1,8 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+// Our program has a new typw called "bot"
+
+// we use interface  -
+// to define a method set or function set. what
+// different function it has and wht return type it has
+type bot interface {
+	// Now,
+	// fro all the custom type, inside the definition
+	// if you are a type in this program with a
+	// function called "getGreeting" and you return a
+	// string then you are now an honorary member
+	// of the type bot
+
+	// no that you're(englishBot, spanishBot) [struct]
+	// also an honorary member of type "bot". eg: -
+	// type englishBot struct{}
+	// type spanishBot struct{}
+	// you can now call this function called "printGreeting"
+
+	// in simple way
+	//
+
+	getGreeting() string
+}
 
 type englishBot struct{}
 type spanishBot struct{}
@@ -10,16 +33,27 @@ type spanishBot struct{}
 func main() {
 
 	eb := englishBot{}
-	// sb := spanishBot{}
+	sb := spanishBot{}
 
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
 
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Print((eb.getGreeting()))
+// As printGreeting(eb) and printGreeting(sb) are different type.
+// and they are honored member as bot
+// So, instead of having different type.
+// printGreeting will allow to let them in side the function
+//
+//
+
+func printGreeting(b bot) {
+	fmt.Println((b.getGreeting()))
 }
+
+// func printGreeting(eb englishBot) {
+// 	fmt.Print((eb.getGreeting()))
+// }
 
 // func printGreeting(sb spanishBot) {
 // 	fmt.Print((sb.getGreeting()))
