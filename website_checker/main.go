afc95c022673ab4e3f6d3af4d3f8ee9c1aaa3111
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-06-09 10:47:23 +0530

    75. Channel Implementation, how to declare a channel

--- website_checker/main.go
+++ website_checker/main.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 )
 
-func main(){
-	links := []string {
+func main() {
+	links := []string{
 		"http://google.com",
 		"http://facebook.com",
 		"http://stackoverflow.com",
@@ -14,15 +14,20 @@ func main(){
 		"http://amazon.org",
 	}
 
+	// creating a channel
+	// c is scoped and available inside main()
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
 }
 
-func checkLink(link string){
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
-	if(err != nil) {
+	if err != nil {
 		fmt.Println(link, "might be down !")
 	}
+	// How can we communicate or using channel
 	fmt.Println(link, "is up!")
 }
